8797c515cbc411907ad838283b93f755744fe2be
Arthur63 <Arthur63@163.com>
2023-12-03 11:08:01 +0800

    feat: add more functions

--- mstr.h
+++ mstr.h
@@ -14,25 +14,58 @@ typedef struct
 } mstr;
 
 extern mstr *mstr_init(mstr *str);
-
-extern size_t mstr_cap(const mstr *str);
-
-extern size_t mstr_len(const mstr *str);
-
-extern const char *mstr_cstr(const mstr *str);
-
+extern mstr *mstr_free(mstr *str);
 extern mstr *mstr_reserve(mstr *dest, size_t cap);
 
+static inline mstr mstr_new(void);
+static inline mstr mstr_new_cap(size_t cap);
+static inline mstr mstr_new_char(char src);
+static inline mstr mstr_new_cstr(const char *src);
+
+extern size_t mstr_cap(const mstr *str);
+extern size_t mstr_len(const mstr *str);
+extern const char *mstr_data(const mstr *str);
+
+extern char *mstr_move_cstr(mstr *str);
+extern mstr *mstr_move_mstr(mstr *dest, mstr *src);
+
 extern mstr *mstr_cat_char(mstr *dest, char src);
-
 extern mstr *mstr_cat_cstr(mstr *dest, const char *src);
-
 extern mstr *mstr_cat_mstr(mstr *dest, const mstr *src);
 
+extern mstr *mstr_assign_char(mstr *dest, char src);
 extern mstr *mstr_assign_cstr(mstr *dest, const char *src);
-
 extern mstr *mstr_assign_mstr(mstr *dest, const mstr *src);
 
-extern void mstr_free(mstr *str);
+static inline mstr mstr_new(void)
+{
+    mstr str;
+    mstr_init(&str);
+    return str;
+}
+
+static inline mstr mstr_new_cap(size_t cap)
+{
+    mstr str;
+    mstr_init(&str);
+    mstr_reserve(&str, cap);
+    return str;
+}
+
+static inline mstr mstr_new_char(char src)
+{
+    mstr str;
+    mstr_init(&str);
+    mstr_assign_char(&str, src);
+    return str;
+}
+
+static inline mstr mstr_new_cstr(const char *src)
+{
+    mstr str;
+    mstr_init(&str);
+    mstr_assign_cstr(&str, src);
+    return str;
+}
 
 #endif
