af40ef98dbf42d5cf329940900a8d6822f94e8cc
Arthur63 <Arthur63@163.com>
2024-01-11 17:53:13 +0800

    rename mstr to mstr_t

--- mstr.h
+++ mstr.h
@@ -4,27 +4,27 @@
 #include <stddef.h>
 #include <threads.h>
 
-struct mstr_heap
+struct mstr_heap_t
 {
   size_t cap;
   size_t len;
   char *data;
 };
 
-struct mstr_sso
+struct mstr_sso_t
 {
   unsigned char flag : 1;
   unsigned char len : 8 * sizeof (unsigned char) - 1;
-  char data[sizeof (struct mstr_heap) - sizeof (unsigned char)];
+  char data[sizeof (struct mstr_heap_t) - sizeof (unsigned char)];
 };
 
-union mstr
+union mstr_t
 {
-  struct mstr_sso sso;
-  struct mstr_heap heap;
+  struct mstr_sso_t sso;
+  struct mstr_heap_t heap;
 };
 
-typedef union mstr mstr;
+typedef union mstr_t mstr_t;
 
 typedef enum
 {
@@ -36,61 +36,62 @@ extern thread_local mstr_errno_t mstr_errno;
 #define MSTR_INIT_CAP 8
 #define MSTR_EXPAN_RATIO 2
 
-extern void mstr_init (mstr *str) __attribute__ ((nonnull (1)));
+extern void mstr_init (mstr_t *str) __attribute__ ((nonnull (1)));
 
-extern void mstr_free (mstr *str) __attribute__ ((nonnull (1)));
+extern void mstr_free (mstr_t *str) __attribute__ ((nonnull (1)));
 
-extern size_t mstr_cap (const mstr *str) __attribute__ ((nonnull (1)));
+extern size_t mstr_cap (const mstr_t *str) __attribute__ ((nonnull (1)));
 
-extern size_t mstr_len (const mstr *str) __attribute__ ((nonnull (1)));
+extern size_t mstr_len (const mstr_t *str) __attribute__ ((nonnull (1)));
 
-extern const char *mstr_data (const mstr *str) __attribute__ ((nonnull (1)));
+extern const char *mstr_data (const mstr_t *str) __attribute__ ((nonnull (1)));
 
-extern mstr *mstr_reserve (mstr *dest, size_t ncap)
+extern mstr_t *mstr_reserve (mstr_t *dest, size_t ncap)
     __attribute__ ((nonnull (1)));
 
-extern char *mstr_release (mstr *str)
+extern char *mstr_release (mstr_t *str)
     __attribute__ ((nonnull (1), warn_unused_result));
 
-extern mstr *mstr_move_mstr (mstr *dest, mstr *src)
+extern mstr_t *mstr_move_mstr (mstr_t *dest, mstr_t *src)
     __attribute__ ((nonnull (1, 2)));
 
-extern void mstr_swap (mstr *dest, mstr *src) __attribute__ ((nonnull (1, 2)));
+extern void mstr_swap (mstr_t *dest, mstr_t *src)
+    __attribute__ ((nonnull (1, 2)));
 
-extern mstr *mstr_cat_char (mstr *dest, char src)
+extern mstr_t *mstr_cat_char (mstr_t *dest, char src)
     __attribute__ ((nonnull (1)));
 
-extern mstr *mstr_cat_cstr (mstr *dest, const char *src)
+extern mstr_t *mstr_cat_cstr (mstr_t *dest, const char *src)
     __attribute__ ((nonnull (1, 2)));
 
-extern mstr *mstr_cat_mstr (mstr *dest, const mstr *src)
+extern mstr_t *mstr_cat_mstr (mstr_t *dest, const mstr_t *src)
     __attribute__ ((nonnull (1, 2)));
 
-extern mstr *mstr_cat_chars (mstr *dest, const char *src, size_t slen)
+extern mstr_t *mstr_cat_chars (mstr_t *dest, const char *src, size_t slen)
     __attribute__ ((nonnull (1, 2)));
 
-extern mstr *mstr_assign_char (mstr *dest, char src)
+extern mstr_t *mstr_assign_char (mstr_t *dest, char src)
     __attribute__ ((nonnull (1)));
 
-extern mstr *mstr_assign_cstr (mstr *dest, const char *src)
+extern mstr_t *mstr_assign_cstr (mstr_t *dest, const char *src)
     __attribute__ ((nonnull (1, 2)));
 
-extern mstr *mstr_assign_mstr (mstr *dest, const mstr *src)
+extern mstr_t *mstr_assign_mstr (mstr_t *dest, const mstr_t *src)
     __attribute__ ((nonnull (1, 2)));
 
-extern mstr *mstr_assign_chars (mstr *dest, const char *src, size_t slen)
+extern mstr_t *mstr_assign_chars (mstr_t *dest, const char *src, size_t slen)
     __attribute__ ((nonnull (1, 2)));
 
-extern mstr *mstr_remove (mstr *dest, size_t spos, size_t slen)
+extern mstr_t *mstr_remove (mstr_t *dest, size_t spos, size_t slen)
     __attribute__ ((nonnull (1)));
 
-extern mstr *mstr_substr (mstr *dest, const mstr *src, size_t spos,
-                          size_t slen) __attribute__ ((nonnull (1, 2)));
+extern mstr_t *mstr_substr (mstr_t *dest, const mstr_t *src, size_t spos,
+                            size_t slen) __attribute__ ((nonnull (1, 2)));
 
-extern int mstr_cmp_cstr (const mstr *lhs, const char *rhs)
+extern int mstr_cmp_cstr (const mstr_t *lhs, const char *rhs)
     __attribute__ ((nonnull (1, 2)));
 
-extern int mstr_cmp_mstr (const mstr *lhs, const mstr *rhs)
+extern int mstr_cmp_mstr (const mstr_t *lhs, const mstr_t *rhs)
     __attribute__ ((nonnull (1, 2)));
 
 #endif
